879743fb1f65ed33da657d2360661dcb22941355
Atanas Chuchulev <achuchulev@Atanass-MacBook-Pro.local>
2019-07-09 12:20:28 +0300

    test

--- main.tf
+++ main.tf
@@ -3,3 +3,16 @@ variable "dms_key" {}
 provider "dmsnitch" {
   api_key = "${var.dms_key}"
 }
+
+terraform {
+
+  ##required_version = ">=0.11.0"
+  backend "remote" {
+        organization     = "atanasc-2"
+
+    workspaces {
+      name = "terraform-provider-dmsnitch"
+      #prefix = "app-"
+    }
+  }
+}
